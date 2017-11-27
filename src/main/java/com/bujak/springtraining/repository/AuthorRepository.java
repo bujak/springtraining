53af22bfa252a2e4026ac85cad74aa91fdc0b306
Tomek Bujakowski <bujakowski@gmail.com>
2017-11-27 23:53:33 +0100

    Add Repository annotations

--- src/main/java/com/bujak/springtraining/repository/AuthorRepository.java
+++ src/main/java/com/bujak/springtraining/repository/AuthorRepository.java
@@ -2,6 +2,8 @@ package com.bujak.springtraining.repository;
 
 import com.bujak.springtraining.model.Author;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AuthorRepository extends CrudRepository<Author, Long>{
 }
--- src/main/java/com/bujak/springtraining/repository/BookRepository.java
+++ src/main/java/com/bujak/springtraining/repository/BookRepository.java
@@ -2,6 +2,8 @@ package com.bujak.springtraining.repository;
 
 import com.bujak.springtraining.model.Book;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends CrudRepository<Book, Long> {
 }
