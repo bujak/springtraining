922762886c5f3db5762dd147e0c7311654ceb7bc
Tomasz Bujakowski <bujakowski@gmail.com>
2017-11-29 22:06:10 +0100

    Fix Publisher repository

--- src/main/java/com/bujak/springtraining/repository/PublisherRepository.java
+++ src/main/java/com/bujak/springtraining/repository/PublisherRepository.java
@@ -1,8 +1,9 @@
 package com.bujak.springtraining.repository;
 
+import com.bujak.springtraining.model.Publisher;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PublisherRepository extends CrudRepository<BookRepository, Long> {
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
 }
